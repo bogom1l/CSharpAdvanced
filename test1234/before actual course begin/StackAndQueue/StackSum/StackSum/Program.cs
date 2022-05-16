197c9d776225bb1752c1f514dbc0c341541c36ae
bogi <bogity123@gmail.com>
2022-05-16 18:16:55 +0300

    added test

---
+++ test1234/before actual course begin/StackAndQueue/StackSum/StackSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "End")
+            {
+                string[] tokens = command.Split();  
+                string cmd = tokens[0];
+
+                if (cmd == "add")
+                {
+                    int firstNum = int.Parse(tokens[1]);
+                    int secondNum = int.Parse(tokens[2]);
+
+                    stack.Push(firstNum);
+                    stack.Push(secondNum);
+                }
+                else if(cmd == "remove")
+                {
+                    int count = int.Parse(tokens[1]);   
+                    if (count <= stack.Count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
