0dfa90fe16e330cf624ee1641c508b6e00fb70ff
bogi <bogity123@gmail.com>
2022-05-16 18:32:38 +0300

    added zad

---
+++ StackAndQueue/Zadacha3/Zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadacha4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("b");
+        }
+    }
+}
