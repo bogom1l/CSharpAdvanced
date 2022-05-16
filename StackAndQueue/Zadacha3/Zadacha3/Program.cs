3e94b7f756b55957225027adc41c81648f8e1d4e
bogi <bogity123@gmail.com>
2022-05-16 18:50:21 +0300

    added task

--- StackAndQueue/Zadacha3/Zadacha3/Program.cs
+++ StackAndQueue/Zadacha3/Zadacha3/Program.cs
@@ -8,8 +8,28 @@ namespace Zadacha3
     {
         static void Main(string[] args)
         {
+            //Matching Brackets
 
-            
+            var stack = new Stack<int>();
+
+            string expression = Console.ReadLine();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+                if (c == '(')
+                {
+                    stack.Push(i);
+                }
+                else if(c == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpression = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+
 
 
 
--- StackAndQueue/Zadacha3/Zadacha4/Program.cs
+++ StackAndQueue/Zadacha3/Zadacha4/Program.cs
@@ -10,3 +10,4 @@ namespace Zadacha4
         }
     }
 }
+
