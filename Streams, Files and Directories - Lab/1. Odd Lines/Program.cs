1a26e19a3cc06f645ee871b6667eb9fca1709816
bogi <bogity123@gmail.com>
2022-05-28 12:05:32 +0300

    pushed execises

---
+++ Streams, Files and Directories - Lab/1. Odd Lines/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1._Odd_Lines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+        }
+
+
+
+
+
+
+    }
+}
