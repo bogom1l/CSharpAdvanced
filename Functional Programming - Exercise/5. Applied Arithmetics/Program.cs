d3146488e945c1d404ebe39a032c8b59d0cd6c16
bogi <bogity123@gmail.com>
2022-05-31 18:58:38 +0300

    pushed exercises

---
+++ Functional Programming - Exercise/5. Applied Arithmetics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _5._Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    numbers = numbers.Select(x => x += 1).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    numbers = numbers.Select(x => x = x * 2).ToList();
+                }
+                else if (command == "subtract")
+                {
+                    numbers = numbers.Select(x => x -= 1).ToList();
+                }
+                else if (command == "print")
+                {
+                    print(numbers);
+                }
+
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ Functional Programming - Exercise/6. Reverse and Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _6._Reverse_and_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int numberToDevide = int.Parse(Console.ReadLine());
+
+            Action<List<int>> removeNumbersFromList = myList => myList.RemoveAll(x => x % numberToDevide == 0);
+            Action<List<int>> reverseList = myList => myList.Reverse();
+            Action<List<int>> printList = myList => Console.WriteLine(string.Join(" ", myList));
+
+            removeNumbersFromList(numbers);
+            reverseList(numbers);
+            printList(numbers);
+
+        }
+    }
+}
