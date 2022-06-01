37f1aceaee09665bcb2eff4b4536d4b11076ea59
bogi <bogity123@gmail.com>
2022-06-01 19:49:16 +0300

    pushed class lab exs

---
+++ Defining Classes - Lab/1. Car/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CarManufacturer;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        Car currCar = new Car();
+
+        currCar.Year = 1992;
+        currCar.Make = "VW";
+        currCar.Model = "MK3";
+
+        Console.WriteLine($"Make: {currCar.Make}\nModel: {currCar.Model}\nYear: {currCar.Year}");
+    }
+}
+
---
+++ Defining Classes - Lab/Defining Classes - Lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Defining_Classes___Lab
+{
+    class Dice
+    {
+        int sides; //field
+        public int Sides //property
+        {
+            get { return sides; } //getter
+            set { sides = value; } //setter
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //sample
+
+            Dice d6 = new Dice();
+            Dice d8 = new Dice();
+
+            d6.Sides = 6;
+
+            Console.WriteLine(d6.Sides);
+            Console.WriteLine(d6);
+        }
+
+    }
+}
