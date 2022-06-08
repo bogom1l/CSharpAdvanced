b62ca6e90e1a59bd28c5ba4dde39c25b1f745c91
bogi <bogity123@gmail.com>
2022-06-08 19:00:55 +0300

    pushed last task

---
+++ Generics - Lab/3.GenericScale/EqualityScale.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public T left { get; set; }
+
+        public T right { get; set; }
+
+        public EqualityScale(T leftEl, T rightEl)
+        {
+            this.left = leftEl;
+            this.right = rightEl;
+        }
+
+        public bool AreEqual()
+        {
+            if (left.Equals(right))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ Generics - Lab/3.GenericScale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> eq = new EqualityScale<int>(5, 5);
+            Console.WriteLine(eq.AreEqual());
+
+            EqualityScale<string> eq2 = new EqualityScale<string>("alo", "da");
+            Console.WriteLine(eq2.AreEqual());
+        }
+    }
+}
